e28679e96d13fe8e43a7363262aba50aab3fb18a
KarryCMT <1570606465@qq.com>
2019-07-07 22:15:55 +0800

    enter

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -4,10 +4,12 @@
 
 <script>
 export default {
-
+    name : 'header'
 }
 </script>
 
-<style>
-
+<style scoped>
+#header{width: 100%;height: 50px;color: #fff;background-color: #e54847;border-bottom: 1px solid #e54847;position: relative;}
+#header h1{font-size: 18px;text-align: center;line-height: 50px;font-weight: normal; }
+#header i{position: absolute;left: 5px;top: 50%;margin-top: -13px;font-size: 26px;}
 </style>
